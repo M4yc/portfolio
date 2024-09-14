1e59df49733a5c610eb47de1034873d37e52d431
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-09-13 23:33:39 -0300

    :pencil: Text: Add Text Experiencias

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,48 @@ function draw() {
 }
 
 draw();
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Dados das experiências
+    const experiences = {
+        'naja-cursos': {
+            title: 'Monitor de Cursos',
+            date: 'Dez 2021 - Mar 2022 (4 Meses)',
+            company: 'Naja Cursos',
+            description: 'Na Naja Cursos, atuei como monitor de aulas nos cursos de informática, incluindo Word, Excel e PowerPoint. Além disso, ministrei aulas sobre montagem e manutenção de computadores, proporcionando uma base sólida para os alunos em tecnologia. Também desempenhei a função de fiscal de provas para a FGV, garantindo a integridade e o bom andamento dos exames.'
+        },
+        'nero': {
+            title: 'Estágio Voluntário',
+            date: 'Jun 2023 - Fev 2024 (9 Meses)',
+            company: 'Nero',
+            description: 'Durante meu tempo no NERO, desempenhei um papel fundamental no projeto e fabricação de placas de circuito impresso. Além disso, desenvolvi código para microcontroladores, o que incluiu a programação de robôs educacionais. Essa experiência me permitiu aprimorar minhas habilidades técnicas e adquirir um conhecimento valioso na integração de hardware e software.'
+        },
+        'sm-marketing': {
+            title: 'Estágio SM Marketing',
+            date: 'Fev 2024 - (Atualmente)',
+            company: 'SM Marketing',
+            description: 'Na SM Marketing, atuo como designer e editor de vídeos. Minha função envolve a criação de designs visuais impactantes e a edição de vídeos para campanhas e conteúdos promocionais. Essa experiência me permite combinar criatividade com habilidades técnicas para entregar materiais de alta qualidade e engajantes.'
+        }
+    };
+
+    // Referências aos elementos DOM
+    const titleElement = document.getElementById('experience-title');
+    const dateElement = document.getElementById('experience-date');
+    const companyElement = document.getElementById('experience-company');
+    const descriptionElement = document.getElementById('experience-description');
+
+    // Adiciona event listeners aos títulos
+    document.querySelectorAll('.company').forEach(item => {
+        item.addEventListener('click', event => {
+            const experienceId = event.currentTarget.getAttribute('data-experience');
+            const experience = experiences[experienceId];
+
+            if (experience) {
+                titleElement.textContent = experience.title;
+                dateElement.textContent = experience.date;
+                companyElement.textContent = experience.company;
+                descriptionElement.textContent = experience.description;
+            }
+        });
+    });
+});
